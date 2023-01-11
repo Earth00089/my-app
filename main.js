7be874e3bf5c5f2c56c8270e882d974c7a356e21
Earth00089 <122431847+Earth00089@users.noreply.github.com>
2023-01-11 14:13:04 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+function Calculator1(){
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+    var sum = parseInt(num1)+parseInt(num2);
+    document.getElementById("result").innerHTML = "result :" + sum
+    alert(sum)
+}
+
+function Calculator2(){
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+    var sum = parseInt(num1)-parseInt(num2);
+    document.getElementById("result").innerHTML = "result :" + sum
+    alert(sum)
+}
+
+function Calculator3(){
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+    var sum = parseInt(num1)*parseInt(num2);
+    document.getElementById("result").innerHTML = "result :" + sum
+    alert(sum)
+}
+
+function Calculator4(){
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+    var sum = parseInt(num1)/parseInt(num2);
+    document.getElementById("result").innerHTML = "result :" + sum
+    alert(sum)
+}
+
+function myalert(){
+    alert("Hello")
+}
